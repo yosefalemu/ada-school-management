1afde84a4134a9e3664ebb8d07af1c19c76385cf
yosef <yosefalemu007@gmail.com>
2026-03-31 22:32:48 +0300

    test: prettier and lint stagged

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,9 @@
+"use client";
+
+import React from "react";
+
 export default function About() {
+    const [_user, _setUser] = React.useState<string | null>(null);
     const _number = 10;
     return <div className="bg-red-500 text-7xl border-2 border-white">About</div>;
 }
