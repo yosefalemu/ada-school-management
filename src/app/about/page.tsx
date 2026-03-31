0da2a6001104bbf3c9435b2f8c0fe792fd01b5ac
yosef <yosefalemu007@gmail.com>
2026-03-31 21:35:11 +0300

    feat: add husky pre-commit hook and lint-staged configuration

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,16 +1,5 @@
-"use client";
-
-import { useState } from "react";
-
-
 export default function About(){
-    const [_state, _setState] = useState();
-    const handleClick = (_age: number) =>{
-    
-
-    }
-    const test = 123
-    const _name = "yosef"
-    console.log(test)
+    const number = 10
+    console.log("number", number)
     return <div className="bg-red-500 text-7xl border-2 border-white">About</div>
 }
