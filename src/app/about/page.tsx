6757b69431b12a21e6eb17198b88d9c889e31eb9
yosef <yosefalemu007@gmail.com>
2026-03-31 22:40:55 +0300

    test: prettier and lint stagged

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,6 +4,7 @@ import React from "react"
 
 export default function About() {
     const [_user, _setUser] = React.useState<string | null>(null)
+    const [_count, _setCount] = React.useState(0)
     const _number = 10
     return <div className="bg-red-500 text-7xl border-2 border-white">About</div>;
 }
